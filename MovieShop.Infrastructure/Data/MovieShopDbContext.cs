6fd8bb34b4ac85d54f60d6df59ab5a33587d849f
nosnatef <gushua1984@gmail.com>
2020-12-02 19:07:44 -0800

    adding migrations

---
+++ MovieShop.Infrastructure/Data/MovieShopDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.EntityFrameworkCore;
+using MovieShop.Core.Entities;
+
+namespace MovieShop.Infrastructure.Data
+{
+    public class MovieShopDbContext : DbContext
+    {
+
+        public MovieShopDbContext(DbContextOptions<MovieShopDbContext> options) : base(options)
+        {
+
+        }
+
+        // add-migration will lock for a class that derives from DbContext class
+        // does that dbcontext class has dbset? If yes it will look for entity of that dbset
+        // By default if any dbset entity has id property then EF will automatically assume it's a primary key
+        // EF will check with the existing Database based on connection string and compare them and generate the migration file
+        public DbSet<Genre> Genres { get; set; }
+
+    }
+}
--- MovieShop/Startup.cs
+++ MovieShop/Startup.cs
@@ -8,6 +8,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MovieShop.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.SqlClient;
 
 namespace MovieShop
 {
@@ -24,6 +27,9 @@ namespace MovieShop
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<MovieShopDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString(("MovieShopDbConnection"))));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
